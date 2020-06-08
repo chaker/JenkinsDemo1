8f1dea217036f00e4cad50e0225ad56adca6510d
chaker.ghanmi@gmail.com <Jendouba2020>
2020-06-08 18:17:47 +0200

    say hello

--- JenkinsDemo1/JenkinsDemo1/Form1.Designer.cs
+++ JenkinsDemo1/JenkinsDemo1/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "btnHelloWord";
+            this.button1.Text = "Say Hello";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
